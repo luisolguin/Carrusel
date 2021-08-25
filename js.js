ccb833cd77ec6ca4e838b0a103678c41b3b5b01c
luisolguin <luisolguinjh@hotmail.com>
2021-08-25 18:14:39 -0500

    trabajo del dia sobre sliders

--- js.js
+++ js.js
@@ -1,17 +1,35 @@
 var slideIndex = 0;
 var timeoutHandle;
-var slideTime=3000;
+var slideTime=6000;
+(function(){
+  const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("Simple-class");
+    for(let i=0;i<slides.length;i++){
+      dots[0].innerHTML+='<span class="dot" onclick="currentSlide('+ i+1 +')">Hi</span>';
+    }
 
+}
+
+)();
 //showSlides();
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  console.log(dots.length);
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  //console.log(slideIndex);
+
+
+
+  slideIndex=(slideIndex+1)%slides.length;
+  dots[slideIndex].className += " active";
+  slides[slideIndex].style.display = "block";
   window.clearTimeout(timeoutHandle);
   timeoutHandle=window.setTimeout(showSlides, slideTime); // Change image every 2 seconds
 }
